a6107df737f7ba9a5af5e4dee457c2d9263c7de6
chardatt <58262163+chardatt@users.noreply.github.com>
2022-02-19 22:48:55 +0100

    Wall Detection Wall Run

--- Parkour_UnityProject/Assets/Script/Wallrun.cs
+++ Parkour_UnityProject/Assets/Script/Wallrun.cs
@@ -13,6 +13,18 @@ public class Wallrun : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        int layerMask = 1 << 8;
+
+        // This would cast rays only against colliders in layer 8.
+        // But instead we want to collide against everything except layer 8. The ~ operator does this, it inverts a bitmask.
+        layerMask = ~layerMask;
+
+        RaycastHit hit;
+        // Does the ray intersect any objects excluding the player layer
+        if (Physics.Raycast(transform.position, transform.forward, out hit, 2.5f, layerMask) || Physics.Raycast(transform.position, transform.right, out hit, 2.5f, layerMask) || Physics.Raycast(transform.position, -transform.right, out hit, 2.5f, layerMask))
+        {
+            Debug.DrawRay(transform.position, hit.point, Color.black);
+            Debug.Log("Did Hit");
+        }
     }
 }

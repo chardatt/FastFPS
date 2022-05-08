4a60ecafdb95f6c46d49fe316eb5d53d80f115d5
chardatt <58262163+chardatt@users.noreply.github.com>
2022-05-08 20:26:25 +0200

    speedline fix

--- RTTB/Assets/Script/SpeedLineController.cs
+++ RTTB/Assets/Script/SpeedLineController.cs
@@ -9,6 +9,8 @@ public class SpeedLineController : MonoBehaviour
     CharacterController playerController;
     [SerializeField]
     VisualEffect visualEffect;
+    float spawnRate;
+    float velocity = 0;
     // Start is called before the first frame update
     void Start()
     {
@@ -19,7 +21,11 @@ public class SpeedLineController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        visualEffect.SetFloat("SpawnRate", 150 * playerController.velocity.magnitude / 30);
-//        Debug.Log(playerController.velocity.magnitude);
+        if (spawnRate > playerController.velocity.magnitude)
+            spawnRate = playerController.velocity.magnitude;
+        else
+            spawnRate = Mathf.SmoothDamp(spawnRate, playerController.velocity.magnitude, ref velocity, 5f);
+        visualEffect.SetFloat("SpawnRate", 150 * spawnRate / 30);
+        Debug.Log(playerController.velocity.magnitude + " " + spawnRate);
     }
 }

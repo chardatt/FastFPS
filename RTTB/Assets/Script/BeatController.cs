8272bacdd5ea7a6899fbfde7dd85a68c50ec9dd5
chardatt <58262163+chardatt@users.noreply.github.com>
2022-05-19 00:48:33 +0200

    LD + Pulse

--- RTTB/Assets/Script/BeatController.cs
+++ RTTB/Assets/Script/BeatController.cs
@@ -1,23 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
 
 public class BeatController : MonoBehaviour
 {
     public PlatformMovement environnementObject;
     public bool notPlaying = false;
     public FMOD.Studio.EventInstance event_fmod;
+    double timer;
+    public Volume postProcessVolume;
+    public Bloom bloom;
+    float velocity = 0;
+    [SerializeField] float bloomSmoothTime;
     void Start()
     {
         event_fmod = FMODUnity.RuntimeManager.CreateInstance("event:/BeatAlexis 2");
+        postProcessVolume.profile.TryGet<Bloom>(out bloom);
     }
 
     void Update()
     {
+        bloom.intensity.value = Mathf.SmoothDamp(bloom.intensity.value, 0.5f, ref velocity, bloomSmoothTime);
         if (environnementObject.moving == true && notPlaying == true)
         {
             event_fmod.start();
             notPlaying = false;
+        }
+        if (!notPlaying)
+        {
+            timer += Time.deltaTime;
+            if (timer >= 0.85714285714)
+            {
+                timer = 0;
+                bloom.intensity.value = 20;
+            }
         }
     }
 
